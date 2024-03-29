5e9400792c859ce3f35bda50278ca1949458dfbc
Ruslan <fedosov.ruslan@gmail.com>
2024-03-29 15:23:38 +0100

    modal false

--- src/components/CompleteRegistrationModal.vue
+++ src/components/CompleteRegistrationModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <TransitionRoot appear :show="true" as="template">
+  <TransitionRoot appear :show="false" as="template">
     <Dialog as="div" @close="closeModal" class="relative z-50">
       <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0" enter-to="opacity-100"
         leave="duration-200 ease-in" leave-from="opacity-100" leave-to="opacity-0">
