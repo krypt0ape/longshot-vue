13bc43189f713d27168844f285db3f91b559b8ef
Ruslan <fedosov.ruslan@gmail.com>
2024-03-29 15:23:18 +0100

    CompleteRegistraionModal start;

--- src/App.vue
+++ src/App.vue
@@ -7,72 +7,69 @@ import AppFooter from "./components/AppFooter.vue";
 import AppSidebar from "./components/AppSidebar.vue";
 import MobileMenu from "./components/MobileMenu.vue";
 import PageInitialStateLoader from "./layouts/PageInitialStateLoader.vue";
+import CompleteRegistrationModal from "@/components/CompleteRegistrationModal.vue";
 
 const route = useRoute();
 
 const topImage = computed(() => {
-	// Remove the top glow effect on the blog single page
-	if (
-		route.path.startsWith("/blog") &&
-		!route.path.includes("category") &&
-		!route.path.includes("casino/games")
-	) {
-		return null;
-	}
-	if (route.path.startsWith("/casino")) {
-		return "/img/PURPLE-GLOW-TOP.png";
-	}
-	return "/img/GREEN-GLOW-TOP.png";
+  // Remove the top glow effect on the blog single page
+  if (
+    route.path.startsWith("/blog") &&
+    !route.path.includes("category") &&
+    !route.path.includes("casino/games")
+  ) {
+    return null;
+  }
+  if (route.path.startsWith("/casino")) {
+    return "/img/PURPLE-GLOW-TOP.png";
+  }
+  return "/img/GREEN-GLOW-TOP.png";
 });
 </script>
 <template>
-	<PageInitialStateLoader>
-		<div class="font-body relative w-screen flex">
-			<div
-				class="fixed bg-red-500 z-50 h-[50px] bottom-0 left-0 right-0 z-50 md:hidden"
-			>
-				<MobileMenu />
-			</div>
-			<AppSidebar />
+  <PageInitialStateLoader>
+    <div class="font-body relative w-screen flex">
+      <div class="fixed bg-red-500 z-50 h-[50px] bottom-0 left-0 right-0 z-50 md:hidden">
+        <MobileMenu />
+      </div>
+      <AppSidebar />
 
-			<section class="relative flex-1">
-				<div class="sticky top-0 left-0 right-0 z-50">
-					<AppHeader />
-				</div>
+      <section class="relative flex-1">
+        <div class="sticky top-0 left-0 right-0 z-50">
+          <AppHeader />
+        </div>
 
-				<div
-					id="page"
-					:class="['flex-1  relative  min-h-screen flex flex-col']"
-				>
-					<div id="main-layout" class="flex-1 relative z-20 min-h-[500px]">
-						<Transition name="page">
-							<RouterView></RouterView>
-						</Transition>
-					</div>
-					<div class="relative z-20">
-						<AppFooter />
-					</div>
-				</div>
-			</section>
-			<aside>
-				<!--  -->
-			</aside>
+        <div id="page" :class="['flex-1  relative  min-h-screen flex flex-col']">
+          <div id="main-layout" class="flex-1 relative z-20 min-h-[500px]">
+            <Transition name="page">
+              <RouterView></RouterView>
+            </Transition>
+          </div>
+          <div class="relative z-20">
+            <AppFooter />
+          </div>
+        </div>
+      </section>
+      <aside>
+        <!--  -->
+      </aside>
 
-			<!-- <PromotionModal />
+      <!-- <PromotionModal />
     <WalletModal /> -->
-		</div>
-	</PageInitialStateLoader>
+      <CompleteRegistrationModal />
+    </div>
+  </PageInitialStateLoader>
 </template>
 <style>
 .page-enter-active,
 .page-leave-active {
-	transition: all 0.4s;
+  transition: all 0.4s;
 }
 
 .page-enter-from,
 .page-leave-to {
-	opacity: 0;
-	filter: blur(1rem);
+  opacity: 0;
+  filter: blur(1rem);
 }
 </style>
 @/stores/useMenuStore
---
+++ src/components/CompleteRegistrationModal.vue
@@ -0,0 +1,125 @@
+<template>
+  <TransitionRoot appear :show="true" as="template">
+    <Dialog as="div" @close="closeModal" class="relative z-50">
+      <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0" enter-to="opacity-100"
+        leave="duration-200 ease-in" leave-from="opacity-100" leave-to="opacity-0">
+        <div class="fixed inset-0 bg-black/25" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 overflow-y-auto">
+        <div class="flex min-h-full items-center justify-center p-4 text-center">
+          <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100" leave="duration-200 ease-in" leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95">
+            <div class="cr-modal rounded-2xl w-1/3">
+              <DialogPanel
+                class="w-full transform overflow-hidden rounded-2xl bg-brand-bodyBg p-6 text-left align-middle shadow-xl transition-all ">
+                <DialogTitle as="h3" class="text-md font-thin text-white">
+                  Complete Registration
+                </DialogTitle>
+                <TransitionRoot :show="step === 1" enter="duration-300 ease-out" enter-from="opacity-0 scale-95"
+                  enter-to="opacity-100 scale-100" leave="duration-200 ease-in" leave-from="opacity-100 scale-100"
+                  leave-to="opacity-0 scale-95">
+                  <div class="mt-2">
+                    <p class="text-lg text-center text-gray-500">
+                      Step 1/2: Fill out your details
+                    </p>
+                    <Input class="mt-1 w-full" placeholder="Please choose a nickname *" />
+                  </div>
+
+                  <div class="mt-4">
+                    <button type="button"
+                      class="w-full inline-flex justify-center rounded-md border border-transparent bg-lime-500 px-4 py-2 text-lg font-bold text-brand-accentStroke hover:bg-green-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      @click="() => submit(1)">
+                      Continue
+                    </button>
+                  </div>
+                </TransitionRoot>
+                <TransitionRoot :show="step === 2" enter="duration-300 ease-out" enter-from="opacity-0 scale-95"
+                  enter-to="opacity-100 scale-100" leave="duration-200 ease-in" leave-from="opacity-100 scale-100"
+                  leave-to="opacity-0 scale-95">
+                  <div class="mt-2">
+                    <p class="text-lg text-center text-gray-500">
+                      <i @click="() => submit(-1)" class="fa-solid fa-arrow-left ml-2 text-white cursor-pointer"
+                        float-left /> Step 2/2: Read and accept the terms and conditions
+                    </p>
+                    <p class="text-sm text-white font-thin">
+                      EULA
+                      <br>...
+                      <br>...
+                      <br>...
+                      <br>...
+                      <br>...
+                      <br>...
+                      <br>...
+                      <br>...
+                      <br>...
+                      <br>...
+                      <br>...
+                      <br>...
+                      <br>...
+                      <br>...
+                      <br>...
+                      <br>...
+                      <br>...
+                      <br>...
+                      <br>...
+                      <br>...
+                      EULA
+                    </p>
+                  </div>
+                  <div class="mt-4">
+                    <button type="button"
+                      class="w-full inline-flex justify-center rounded-md border border-transparent bg-lime-500 px-4 py-2 text-lg font-bold text-brand-accentStroke hover:bg-green-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      @click="() => submit()">
+                      Play Now
+                    </button>
+                  </div>
+                </TransitionRoot>
+              </DialogPanel>
+            </div>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+</template>
+<script setup>
+import { computed, ref, onMounted } from "vue";
+import Input from "@/components/Form/Input.vue";
+import useUserStore from "@/stores/useUserStore";
+import {
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from '@headlessui/vue'
+
+const store = useUserStore();
+
+const user = ref()
+const step = ref(1)
+
+onMounted(async () => {
+  console.log(store)
+  user.value = await store.getUser()
+  console.log(store)
+})
+
+const submit = (s) => {
+  step.value += s
+}
+
+const show = computed(() => {
+  return store?.user?.registration !== 'COMPLETE';
+});
+
+</script>
+
+<style lang="scss" scoped>
+.cr-modal {
+  background: linear-gradient(180deg, #FA00FF 0%, #6A2B74 100%);
+  padding: 2px;
+}
+</style>
