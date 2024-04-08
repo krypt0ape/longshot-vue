6291a843e36e737c11536b849e8332fddfc76a67
Ruslan <fedosov.ruslan@gmail.com>
2024-04-08 16:22:43 +0200

    broken useKyc;

--- src/components/WalletRiskKycCheck.vue
+++ src/components/WalletRiskKycCheck.vue
@@ -1,3 +1,22 @@
 <template>
-  WalletRiskKycCheck content
+  <div class="mx-10 my-6 p-6 bg-blue-100 border-t-4 border-blue-500 rounded-b text-blue-900 shadow-md" role="alert">
+    <div class="flex">
+      <div class="pr-4"><i class="fa-duotone fa-circle-info"></i></div>
+      <div>
+        <p class="font-bold">KYC Verification</p>
+        <p class="text-sm">Your credentials require us to walk you through KYC verification</p>
+      </div>
+      <PrimaryButton class="ml-auto mr-0" @click="start">Launch KYC</PrimaryButton>
+    </div>
+  </div>
+  <div id="sumsub-websdk-container"></div>
 </template>
+
+<script setup>
+import useKyc from "@/composables/useKyc";
+import useAsync from '@/composables/useAsync';
+import PrimaryButton from '@/components/Buttons/PrimaryButton.vue'
+
+const { start } = useKyc();
+
+</script>
---
+++ src/composables/useKyc.js
@@ -0,0 +1,65 @@
+import snsWebSdk from "@sumsub/websdk";
+import useApi from "./useApi";
+import { useAuth0 } from "@auth0/auth0-vue";
+
+export default function useKyc() {
+  const { getAccessTokenSilently, isAuthenticated } = useAuth0();
+
+  /**
+   * @param accessToken - access token that you generated on the backend in Step 2
+   * @param applicantEmail - applicant email (not required)
+   * @param applicantPhone - applicant phone, if available (not required)
+   * @param customI18nMessages - customized locale messages for current session (not required)
+   */
+  const launchWebSdk = (
+    accessToken,
+    applicantEmail = null,
+    applicantPhone = null
+  ) => {
+    const snsWebSdkInstance = snsWebSdk
+      .init(
+        accessToken,
+        // token update callback, must return Promise
+        // Access token expired
+        // get a new one and pass it to the callback to re-initiate the WebSDK
+        () => this.getNewAccessToken()
+      )
+      .withConf({
+        lang: "en", //language of WebSDK texts and comments (ISO 639-1 format)
+        email: applicantEmail,
+        phone: applicantPhone,
+        theme: "dark" | "light",
+      })
+      .withOptions({ addViewportTag: false, adaptIframeHeight: true })
+      // see below what kind of messages WebSDK generates
+      .on("idCheck.onStepCompleted", (payload) => {
+        console.log("onStepCompleted", payload);
+      })
+      .on("idCheck.onError", (error) => {
+        console.log("onError", error);
+      })
+      .build();
+
+    // you are ready to go:
+    // just launch the WebSDK by providing the container element for it
+    snsWebSdkInstance.launch("#sumsub-websdk-container");
+  };
+
+  const getNewAccessToken = async () => {
+    const { data, refetch } = useApi("post", "/kyc/get-access-token");
+
+    await refetch();
+    const { token } = data;
+    console.log(data);
+    return token;
+  };
+
+  const start = async () => {
+    const accessToken = await getNewAccessToken();
+    launchWebSdk(accessToken);
+  };
+
+  return {
+    start,
+  };
+}
