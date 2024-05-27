ab50e9381fce29a5475c53b67c376591fd04a260
Ruslan <fedosov.ruslan@gmail.com>
2024-05-27 15:16:40 +0200

    sportsbook: batch bet updates;

--- src/components/SportsEventMarketLineOutcome.vue
+++ src/components/SportsEventMarketLineOutcome.vue
@@ -6,72 +6,77 @@ import useFormatOdds from "@/composables/useFormatOdds";
 const store = useBetslipStore();
 
 const props = defineProps({
-	marketLineOutcome: {
-		type: Object,
-		required: true,
-	},
-	competitors: {
-		type: Array,
-		required: true,
-	},
-	marketLabel: {
-		type: String,
-		required: true,
-	},
-	marketId: {
-		type: Number,
-		required: true,
-	},
-	event: {
-		type: Object,
-		required: true,
-	},
-	specifiers: {
-		type: String,
-		required: false,
-	},
+  marketLineOutcome: {
+    type: Object,
+    required: true,
+  },
+  competitors: {
+    type: Array,
+    required: true,
+  },
+  marketLabel: {
+    type: String,
+    required: true,
+  },
+  marketId: {
+    type: Number,
+    required: true,
+  },
+  event: {
+    type: Object,
+    required: true,
+  },
+  specifiers: {
+    type: String,
+    required: false,
+  },
 });
 
 const formatOdds = useFormatOdds(props.marketLineOutcome.odds);
 
 const addToBetslip = () => {
-	store.addToBetslip({
-		sportEventId: props.event.id,
-		sportEventLabel: eventName(props.event),
-		marketId: parseInt(props.marketId),
-		marketLabel: marketName(props.marketLabel, props.event.competitors),
-		outcomeId: parseInt(props.marketLineOutcome.outcomeId),
-		outcomeLabel: outcomeLabel.value,
-		odds: parseFloat(props.marketLineOutcome.odds),
-	});
+  store.addToBetslip({
+    id: props.marketLineOutcome.id,
+    specifiers: props.marketLineOutcome.specifiers,
+    sportEventId: props.event.id,
+    sportEventLabel: eventName(props.event),
+    marketId: parseInt(props.marketId),
+    marketLabel: marketName(props.marketLabel, props.event.competitors),
+    outcomeId: parseInt(props.marketLineOutcome.outcomeId),
+    outcomeLabel: outcomeLabel.value,
+    odds: parseFloat(props.marketLineOutcome.odds),
+  });
 };
 
 const outcomeLabel = computed(() => {
-	return marketName(props.marketLineOutcome.outcome.name, props.competitors, props.specifiers);
+  return marketName(
+    props.marketLineOutcome.outcome.name,
+    props.competitors,
+    props.specifiers
+  );
 });
-
 </script>
 <template>
-	<div
-		@click="addToBetslip"
-		class="cursor-pointer hover:bg-brand-accentBgHeader transition sports-event-market-line-outcome flex justify-between py-[12px] px-[16px] w-full"
-	>
-		<p
-			v-if="marketLineOutcome.outcome"
-			class="text-brand-lightGrey font-semibold"
-		>
-			{{ outcomeLabel }}
-		</p>
-		<!-- TODO user preference odds format -->
-		<p class="green-gradient-text font-semibold">
-			{{ formatOdds.odds.value }}
-		</p>
-	</div>
+  <div
+    @click="addToBetslip"
+    class="cursor-pointer hover:bg-brand-accentBgHeader transition sports-event-market-line-outcome flex justify-between py-[12px] px-[16px] w-full"
+  >
+    <p
+      v-if="marketLineOutcome.outcome"
+      class="text-brand-lightGrey font-semibold"
+    >
+      {{ outcomeLabel }}
+    </p>
+    <!-- TODO user preference odds format -->
+    <p class="green-gradient-text font-semibold">
+      {{ formatOdds.odds.value }}
+    </p>
+  </div>
 </template>
 <style>
 .sports-event-market-line-outcome {
-	border-radius: 10px;
-	border: 1px solid #273646;
-	background: #071420;
+  border-radius: 10px;
+  border: 1px solid #273646;
+  background: #071420;
 }
 </style>
--- src/stores/useBetslipStore.js
+++ src/stores/useBetslipStore.js
@@ -2,72 +2,86 @@ import { defineStore } from "pinia";
 import { buildBetKey } from "@/utils/buildBetKey";
 import { computed, ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
+import useAsyncApi from "@/composables/useAsyncApi";
 
 export default defineStore("betslip", () => {
-	const route = useRoute();
-	const router = useRouter();
-	
-	const betslipOpen = computed(() => {
-		return route.query.betslip === "open";
-	});
+  const route = useRoute();
+  const router = useRouter();
 
-	const toggleBetslip = () => {
-		if (betslipOpen.value) {
-			// close modal
-			router.replace({ query: { } });
-		} else {
-			// open modal
-			router.replace({ query: { betslip: "open" } });
-		}
-	 };
+  const { loading, error, data, call } = useAsyncApi("post", "/sportsbook/bet");
 
-	 const betslip = ref({});
+  const betslipOpen = computed(() => {
+    return route.query.betslip === "open";
+  });
 
-	 const postBets = async () => { 
+  const toggleBetslip = () => {
+    if (betslipOpen.value) {
+      // close modal
+      router.replace({ query: {} });
+    } else {
+      // open modal
+      router.replace({ query: { betslip: "open" } });
+    }
+  };
 
-	 };
+  const betslip = ref({});
 
-	 const addToBetslip = (data) => {
-		const {sportEventId, sportEventLabel, outcomeId, outcomeLabel, marketId, marketLabel, odds} = data;
-		// Create a unique key for the bet object 
-		const key = buildBetKey(sportEventId, outcomeId, marketId);
-		// If the bet is already in the betslip, 
-		// remove it
-		if(betslip.value[key]) {
-			removeBet(key);
-			return;
-		};
-		// Add the bet to the betslip
-		 betslip.value[key] = {
-			 sportEventId,
-			 outcomeId,
-			 marketId,
-			 sportEventLabel,
-			 outcomeLabel,
-			 marketLabel,
-			 amount: 0,
-			 odds
-		 }
-		 if(! betslipOpen.value){
-			toggleBetslip();
-		 }
-	 }
+  const postBets = async () => {
+    await call({ data: betslip.value });
+  };
 
-	 const removeBet = (betKey) => { 
-		 delete betslip.value[betKey];
-	 }
+  const addToBetslip = (data) => {
+    const {
+      id,
+      sportEventId,
+      sportEventLabel,
+      outcomeId,
+      outcomeLabel,
+      marketId,
+      marketLabel,
+      odds,
+    } = data;
+    // Create a unique key for the bet object
+    const key = buildBetKey(sportEventId, outcomeId, marketId);
+    // If the bet is already in the betslip,
+    // remove it
+    if (betslip.value[key]) {
+      removeBet(key);
+      return;
+    }
+    // Add the bet to the betslip
+    betslip.value[key] = {
+      id,
+      sportEventId,
+      outcomeId,
+      marketId,
+      sportEventLabel,
+      outcomeLabel,
+      marketLabel,
+      amount: 100,
+      ISO: "USD",
+      odds,
+    };
+    if (!betslipOpen.value) {
+      toggleBetslip();
+    }
+  };
 
-	 const updateBetAmount = ({betKey, amount}) => { 
-		 betslip.value[betKey].amount = amount;
-	 }
+  const removeBet = (betKey) => {
+    delete betslip.value[betKey];
+  };
 
-	 return {
-		betslipOpen,
-		toggleBetslip,
-		betslip,
-		postBets,
-		addToBetslip,
-		removeBet,
-		updateBetAmount
-	 }
+  const updateBetAmount = ({ betKey, amount }) => {
+    betslip.value[betKey].amount = amount;
+  };
+
+  return {
+    betslipOpen,
+    toggleBetslip,
+    betslip,
+    postBets,
+    addToBetslip,
+    removeBet,
+    updateBetAmount,
+  };
 });
