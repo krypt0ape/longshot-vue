6e84d28ae6943fc524eff6fbbc2a4e1d785f08e4
Ruslan <fedosov.ruslan@gmail.com>
2024-05-28 14:28:06 +0200

    specifiers fix;

--- src/components/SportsEventMarketCard.vue
+++ src/components/SportsEventMarketCard.vue
@@ -1,53 +1,70 @@
 <script setup>
 import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
 import SportsEventMarket from "@/components/SportsEventMarket.vue";
-import {marketName} from "@/utils/radarHelpers"
-import { computed} from "vue";
+import { marketName } from "@/utils/radarHelpers";
+import { computed } from "vue";
 
 const props = defineProps({
-	market: {
-		type: Object,
-		required: true,
-	},
-	event: {
-		type: Object,
-		required: true,
-	},
+  market: {
+    type: Object,
+    required: true,
+  },
+  event: {
+    type: Object,
+    required: true,
+  },
 });
 
+const specifier = computed(() => {
+  return props.market.marketLines[0].specifiers;
+});
 
-const specifier = computed(()=>{
-	return props.market.marketLines[0].specifiers
-})
-
-const status = computed(()=>{
-	return props.market.marketLines[0].status
-})
+const status = computed(() => {
+  return props.market.marketLines[0].status;
+});
 </script>
 <template>
-	<div class="sports-event-market-card my-6">
-		<Disclosure v-slot="{ open }">
-			<DisclosureButton class="px-[32px] py-[15px]  w-full">
-				<div class="flex justify-between items-center">
-					<p class="font-medium text-lg">{{ marketName(market.name, event.competitors, specifier) }} - ID: {{ market.id }} - Status: {{status}}</p>
-					<i :class="[open ? 'fa-chevron-down' : 'fa-chevron-left', 'fa-solid  text-white']"></i>
-				</div>
-			</DisclosureButton>
-			<DisclosurePanel static   class="text-gray-500 px-[20px] py-[20px] sports-event-market-card-panel">
-				<SportsEventMarket :market="market" :event="event"/>
-				<!-- <pre>{{ market }}</pre> -->
-			</DisclosurePanel>
-		</Disclosure>
-	</div>
+  <div class="sports-event-market-card my-6">
+    <Disclosure v-slot="{ open }">
+      <DisclosureButton class="px-[32px] py-[15px] w-full">
+        <div
+          class="flex justify-between items-center"
+          :class="{
+            'bg-yellow-500': specifier && !specifier?.includes('variant='),
+            'bg-red-500': specifier,
+          }"
+        >
+          <p class="font-medium text-lg">
+            {{ specifier }} - specifier <br />
+            {{ marketName(market.name, event.competitors, specifier) }} - ID:
+            {{ market.id }} - Status: {{ status }}
+          </p>
+          <i
+            :class="[
+              open ? 'fa-chevron-down' : 'fa-chevron-left',
+              'fa-solid  text-white',
+            ]"
+          ></i>
+        </div>
+      </DisclosureButton>
+      <DisclosurePanel
+        static
+        class="text-gray-500 px-[20px] py-[20px] sports-event-market-card-panel"
+      >
+        <SportsEventMarket :market="market" :event="event" />
+        <!-- <pre>{{ market }}</pre> -->
+      </DisclosurePanel>
+    </Disclosure>
+  </div>
 </template>
 <style>
 .sports-event-market-card {
-	border-radius: 5px;
-	border: 1px solid rgba(61, 75, 89, 0.5);
-	background: linear-gradient(335deg, #171f2b 24.05%, #2c3e4e 61.46%);
-	box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.15);
+  border-radius: 5px;
+  border: 1px solid rgba(61, 75, 89, 0.5);
+  background: linear-gradient(335deg, #171f2b 24.05%, #2c3e4e 61.46%);
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.15);
 }
 .sports-event-market-card-panel {
-	border-top: 1px solid rgba(61, 75, 89, 0.5);
+  border-top: 1px solid rgba(61, 75, 89, 0.5);
 }
 </style>
--- src/components/SportsEventMarketLineOutcome.vue
+++ src/components/SportsEventMarketLineOutcome.vue
@@ -6,93 +6,99 @@ import useFormatOdds from "@/composables/useFormatOdds";
 const store = useBetslipStore();
 
 const props = defineProps({
-	marketLineOutcome: {
-		type: Object,
-		required: true,
-	},
-	competitors: {
-		type: Array,
-		required: true,
-	},
-	marketLabel: {
-		type: String,
-		required: true,
-	},
-	marketId: {
-		type: Number,
-		required: true,
-	},
-	event: {
-		type: Object,
-		required: true,
-	},
-	specifiers: {
-		type: String,
-		required: false,
-	},
-	suspended: {
-		type: Boolean,
-		required: false,
-	},
+  marketLineOutcome: {
+    type: Object,
+    required: true,
+  },
+  competitors: {
+    type: Array,
+    required: true,
+  },
+  marketLabel: {
+    type: String,
+    required: true,
+  },
+  marketId: {
+    type: Number,
+    required: true,
+  },
+  event: {
+    type: Object,
+    required: true,
+  },
+  specifiers: {
+    type: String,
+    required: false,
+  },
+  suspended: {
+    type: Boolean,
+    required: false,
+  },
 });
 
 const formatOdds = useFormatOdds(props.marketLineOutcome.odds);
 
 const addToBetslip = () => {
-	if(props.suspended) return;
-	store.addToBetslip({
-		id: props.marketLineOutcome.id,
-		specifiers: props.marketLineOutcome.specifiers,
-		sportEventId: props.event.id,
-		sportEventLabel: eventName(props.event),
-		marketId: parseInt(props.marketId),
-		marketLabel: marketName(props.marketLabel, props.event.competitors),
-		outcomeId: parseInt(props.marketLineOutcome.outcomeId),
-		outcomeLabel: outcomeLabel.value,
-		odds: parseFloat(props.marketLineOutcome.odds),
-	});
+  if (props.suspended) return;
+  store.addToBetslip({
+    id: props.marketLineOutcome.id,
+    specifiers: props.marketLineOutcome.specifiers,
+    sportEventId: props.event.id,
+    sportEventLabel: eventName(props.event),
+    marketId: parseInt(props.marketId),
+    marketLabel: marketName(
+      props.marketLabel,
+      props.event.competitors,
+      props.specifiers
+    ),
+    outcomeId: parseInt(props.marketLineOutcome.outcomeId),
+    outcomeLabel: outcomeLabel.value,
+    odds: parseFloat(props.marketLineOutcome.odds),
+  });
 };
 
 const outcomeLabel = computed(() => {
-	if(! props.marketLineOutcome.outcome) return '';
-	const label =  marketName(
-		props.marketLineOutcome.outcome.name,
-		props.competitors,
-		props.specifiers
-	);
-	return label.charAt(0).toUpperCase() + label.slice(1);
+  if (!props.marketLineOutcome.outcome) return "";
+  const label = marketName(
+    props.marketLineOutcome.outcome.name,
+    props.competitors,
+    props.specifiers
+  );
+  return label.charAt(0).toUpperCase() + label.slice(1);
 });
 </script>
 <template>
-	<div
-		@click="addToBetslip"
-		class="sports-event-market-line-outcome py-[12px] px-[16px] w-full"
-		:class="[
-			suspended ? 'cursor-not-allowed' : 'cursor-pointer hover:bg-brand-accentBgHeader transition ',
-		]"
-		v-tippy="{ content: outcomeLabel, placement: 'top' }"
-	>
-		<div v-if="suspended" class="flex justify-between">
-			<p class="text-brand-darkGrey font-semibold">Suspended</p>
-		</div>
-		<div  v-else class="flex justify-between" >
-			<p
-				v-if="marketLineOutcome.outcome"
-				class="text-brand-lightGrey font-semibold truncate pr-2"
-			>
-				{{ outcomeLabel }}
-			</p>
-			<!-- TODO user preference odds format -->
-			<p class="green-gradient-text font-semibold">
-				{{ formatOdds.odds.value }}
-			</p>
-		</div>
-	</div>
+  <div
+    @click="addToBetslip"
+    class="sports-event-market-line-outcome py-[12px] px-[16px] w-full"
+    :class="[
+      suspended
+        ? 'cursor-not-allowed'
+        : 'cursor-pointer hover:bg-brand-accentBgHeader transition ',
+    ]"
+    v-tippy="{ content: outcomeLabel, placement: 'top' }"
+  >
+    <div v-if="suspended" class="flex justify-between">
+      <p class="text-brand-darkGrey font-semibold">Suspended</p>
+    </div>
+    <div v-else class="flex justify-between">
+      <p
+        v-if="marketLineOutcome.outcome"
+        class="text-brand-lightGrey font-semibold truncate pr-2"
+      >
+        {{ outcomeLabel }}
+      </p>
+      <!-- TODO user preference odds format -->
+      <p class="green-gradient-text font-semibold">
+        {{ formatOdds.odds.value }}
+      </p>
+    </div>
+  </div>
 </template>
 <style>
 .sports-event-market-line-outcome {
-	border-radius: 10px;
-	border: 1px solid #273646;
-	background: #071420;
+  border-radius: 10px;
+  border: 1px solid #273646;
+  background: #071420;
 }
 </style>
