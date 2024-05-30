1d712714017aa3c6032bae8e1ab198d8f1ebaee9
Krypt0 Ape <krypt0.ape1@gmail.com>
2024-05-30 10:18:37 +0100

    bgufix

--- src/stores/useBetslipStore.js
+++ src/stores/useBetslipStore.js
@@ -87,8 +87,8 @@ export default defineStore("betslip", () => {
 		};
 		if (!betslipOpen.value) {
 			toggleBetslip();
-			changeBetSlipTab('betslip');
-			changeBetSlipSubTab('single');
+			// changeBetSlipTab('betslip');
+			// changeBetSlipSubTab('single');
 		}
 	};
 
